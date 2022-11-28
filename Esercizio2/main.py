36141261c1187e9b5a54faf23b9de41ca5c062b7
aismam <ismamabu25@gmail.com>
2022-11-28 19:18:02 +0100

    fixed random forest

--- Esercizio2/main.py
+++ Esercizio2/main.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pmdarima as pm
 import statsmodels.api as sm
@@ -8,6 +9,8 @@ from sklearn.metrics import mean_absolute_error
 from keras.models import Sequential  # pip install keras
 from keras.layers import Dense  # pip install tensorflow (as administrator)
 import math
+from sklearn.metrics import mean_squared_error
+from collections import deque
 
 # # # # # STATISTIC MODELS # # # # #
 
@@ -71,8 +74,8 @@ yfore = fitted.predict(n_periods=12)  # forecast (out-of-sample)
 
 plt.plot(train, color="green")  # Dati di train
 plt.plot(test, color="royalblue")
-plt.plot([None for x in range(12)] + [x for x in ypred[12:]], color="orange")
-plt.plot([None for x in ypred] + [x for x in yfore], color="red")  # Previsione
+plt.plot([None for x in ypred] + [x for x in yfore], color="red")  # forecast
+plt.plot([None for x in range(12)] + [x for x in ypred[12:]], color="orange")  # prediction
 
 plt.xlabel('time')
 plt.ylabel('log amounts')
@@ -109,10 +112,27 @@ yfore = sfit.get_forecast(steps=36)
 expdata = np.exp(ypred)  # unlog
 expfore = np.exp(yfore.predicted_mean)
 
+# MSE & RMSE on train set
+mse_train = mean_squared_error(expdata[1:], train_raw)
+rmse_train = math.sqrt(mse_train)
+print('Train Score - SARIMAX | MSE: {0:0.3f} | RMSE: ({1:0.3f})'.format(mse_train, rmse_train))
+
+# MSE & RMSE on test set
+print(len([None for x in expdata] + [x for x in expfore]), len(test_raw))
+tmp_none = [None for x in expdata] + [x for x in expfore]
+tmp_clear = []
+for elem in tmp_none:
+    if elem != None:
+        tmp_clear.append(elem)
+print(len(tmp_clear), len(test_raw[:36]))
+mse_test = mean_squared_error(tmp_clear, test_raw[:36])
+rmse_test = math.sqrt(mse_test)
+print('Test Score - SARIMAX | MSE: {0:0.3f} | RMSE: ({1:0.3f})'.format(mse_test, rmse_test))
+
 plt.plot(train_raw, color="green")  # raw train data
 plt.plot(test_raw, color="royalblue")
-plt.plot([None for x in expdata] + [x for x in expfore], color="red")
 plt.plot(expdata, color="orange")
+plt.plot([None for x in expdata] + [x for x in expfore], color="red")
 
 plt.xlabel('time')
 plt.ylabel('amounts')
@@ -142,8 +162,8 @@ ypred = fitted.predict_in_sample()  # prediction (in-sample)
 yfore = fitted.predict(n_periods=24)  # forecast (out-of-sample)
 
 plt.plot(log_data, color="green")  # Dati di train
-plt.plot([None for x in range(24)] + [x for x in ypred[24:]], color="orange")  # Forecast
 plt.plot([None for x in ypred] + [x for x in yfore], color="red")  # Predict
+plt.plot([None for x in range(24)] + [x for x in ypred[24:]], color="orange")  # Forecast
 
 plt.xlabel('time')
 plt.ylabel('log amounts')
@@ -158,8 +178,8 @@ expdata = np.exp(yplog)  # unlog
 expfore = np.exp(yfore)
 
 plt.plot(amount, color="green")  # train set grezzo
-plt.plot([None for x in expdata] + [x for x in expfore], color="red")  # previsione
 plt.plot([None for x in range(24)] + [x for x in expdata[24:]], color="orange")
+plt.plot([None for x in expdata] + [x for x in expfore], color="red")  # forecast
 
 plt.xlabel('time')
 plt.ylabel('amounts')
@@ -180,9 +200,14 @@ yfore = sfit.get_forecast(steps=24)
 expdata = np.exp(ypred)  # unlog
 expfore = np.exp(yfore.predicted_mean)
 
-plt.plot(expdata, color="orange")
-plt.plot(amount, color="green")
+# MSE & RMSE on full data set
+mse_train = mean_squared_error(expdata[1:], amount)
+rmse_train = math.sqrt(mse_train)
+print('Full dataset score - SARIMAX | MSE: {0:0.3f} | RMSE: ({1:0.3f})'.format(mse_train, rmse_train))
+
 plt.plot([None for x in expdata] + [x for x in expfore], color="red")
+plt.plot(amount, color="green")
+plt.plot(expdata, color="orange")
 
 plt.xlabel('time')
 plt.ylabel('amounts')
@@ -190,6 +215,75 @@ plt.legend(['Forecast', 'Raw train data', 'Prediction'])
 plt.title('SARIMAX with entire reconstructed data set')
 
 plt.show()
+
+# # # # # MACHINE LEARNING METHODS # # # # #
+
+periods = 12
+forcast = 24
+
+ds = pd.read_csv('serieFcast2021.csv')
+ds = ds[['amount']]
+ds = ds.interpolate()
+logged = np.log(ds)
+
+# ds.plot()
+# plt.show()
+# logged.plot()
+# plt.show()
+
+mod_1 = logged.copy(deep=True)
+
+# Aggiungo 12 colonne dove ognuna è il diff della riga n periodi prima
+for i in range(periods, 0, -1):
+    mod_1[f'amount_{i}'] = mod_1['amount'].diff(periods=i)
+# elimino le prime 12 righe perchè contengono i NaN generati dall'operazione precedente
+# print(mod_1)
+mod_1 = mod_1.dropna()
+# print(mod_1)
+
+x = mod_1.drop(columns=['amount']).values
+y = mod_1['amount'].values
+
+model = RandomForestRegressor().fit(x, y)
+
+forcasts = []
+coda = deque(x[-1], maxlen=periods)
+
+# Uso l'ultima riga per fare la previsione successiva
+# Itero questa procedura per 24 volte e ogni volta rimuovo l'elemento più vecchio e metto la mia nuova y
+for _ in range(forcast):
+    y = model.predict([coda])[0]
+    forcasts.append(y)
+    coda.append(y)
+
+# print(forcasts)
+# print(coda)
+
+reversed = np.exp(forcasts)
+indexes = range(ds.index[-1] + 1, ds.index[-1] + forcast + 1)
+forcast_series = pd.Series(reversed, index=indexes)
+
+predict = np.exp(model.predict(x))
+
+ds['amount'].plot()
+forcast_series.plot()
+plt.plot(predict)
+
+
+plt.xlabel('time')
+plt.ylabel('amounts')
+plt.legend(['Dataset', 'Forecast','Predict'])
+plt.title('Random Forest Regressor with full dataset')
+
+plt.show()
+
+# MSE & RMSE
+raw_data = ds["amount"]
+raw_data = raw_data[:240]
+
+mse = mean_absolute_error(raw_data.values, predict)
+rmse = math.sqrt(mse)
+print('Full dataset score | Random Forest Regressor | MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(mse, rmse))
 
 
 # # # # # PREDICTIVE NEURAL METHODS # # # # #
@@ -237,10 +331,9 @@ model.fit(trainX, trainY, epochs=100, batch_size=2, verbose=2)
 
 # Estimate model performance
 trainScore = model.evaluate(trainX, trainY, verbose=0)
-print('Train Score: MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(trainScore,
-                                                           math.sqrt(trainScore)))
+print('Train Score - MLP train | MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(trainScore, math.sqrt(trainScore)))
 testScore = model.evaluate(testX, testY, verbose=0)
-print('Test Score: MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(testScore, math.sqrt(testScore)))
+print('Test Score - MLP test | MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(testScore, math.sqrt(testScore)))
 # generate predictions for training and forecast for plotting
 trainPredict = model.predict(trainX)
 testForecast = model.predict(testX)
@@ -252,15 +345,18 @@ for n in np.linspace(0, 23, 24):
                                                dtype="float32")))
 
 plt.plot(dataset)
-plt.plot(np.concatenate((np.full(look_back-1, np.nan), trainPredict[:,0])))
-plt.plot(np.concatenate((np.full(len(train)-1, np.nan), testForecast[:, 0])))
-plt.plot(np.concatenate((np.full(len(train)+11, np.nan), result[:, 0])))
+plt.plot(np.concatenate((np.full(look_back - 1, np.nan), trainPredict[:, 0])))
+plt.plot(np.concatenate((np.full(len(train) - 1, np.nan), testForecast[:, 0])))
+plt.plot(np.concatenate((np.full(len(train) + 11, np.nan), result[:, 0])))
 
 plt.xlabel('time')
 plt.ylabel('amounts')
 plt.legend(['Dataset', 'Train Predict', 'Test Forecast', 'Prediction'])
-plt.title('SARIMAX with entire reconstructed data set')
+plt.title('Multilayer Perceptron Model')
 
 plt.show()
 
-print("MSE={}".format(mean_absolute_error(testY, testForecast)))
+mse_mlp_full = mean_absolute_error(testY, testForecast)
+rmse_mlp_full = math.sqrt(mse_mlp_full)
+
+print('Full dataset score | MLP | MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(mse_mlp_full, rmse_mlp_full))
